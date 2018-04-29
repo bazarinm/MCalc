864af6a30b46f22dd1bca30592edc00e19471b58
Михаил Базарин <mabazarin@edu.hse.ru>
2018-04-30 00:27:06 +0300

    add function overload support

--- MCalc/Function.cpp
+++ MCalc/Function.cpp
@@ -7,36 +7,39 @@
 Function::Function(std::string name)
 {
     if (name == "det") {
-        _arity = 1;
         _priority = 0;
-        _argument_types = { Variable::MATRIX }; 
-        _function = [](std::vector<Variable&> args) -> Variable {
-            return Operator::determinant(args[0].getMatrix());
+        _argument_types[{ Variable::MATRIX }] = true; 
+
+        _function[{ Variable::MATRIX }] = [](std::vector<Variable> args) -> Variable {
+            return Operator::determinant(args[0].matrix());
         };
     }
     else if (name == "+") {
-        _arity = 2;
         _priority = 0;
-        _argument_types = { Variable::MATRIX, Variable::MATRIX };
-        _function = [](std::vector<Variable&> args) -> Variable {
-            return Operator::plus(args[0].getMatrix(), args[1].getMatrix());
+        _argument_types[{ Variable::MATRIX, Variable::MATRIX }] = true;
+        _argument_types[{ Variable::SCALAR, Variable::SCALAR }] = true;
+
+        _function[{ Variable::MATRIX, Variable::MATRIX }] = [](std::vector<Variable> args) -> Variable {
+            return Operator::plus(args[0].matrix(), args[1].matrix());
+        };
+        _function[{ Variable::SCALAR, Variable::SCALAR }] = [](std::vector<Variable> args) -> Variable {
+            return args[0].scalar() + args[1].scalar();
         };
     }
 
     //AND SO ON. ANY NEW FUNCTION MUST BE DESCRIBED HERE.
 }
 
-Variable Function::operator()(std::vector<Variable&> args) const {
-    if (args.size() != _arity)
-        throw std::exception("A wrong number of parameters");
+Variable Function::operator()(std::vector<Variable> args) {
+    /*if (args.size() != _arity)
+        throw std::exception("Wrong number of parameters");*/
 
-    unsigned counter = 0;
-    for (Variable& arg : args) {
-        if (arg.getType() != _argument_types[counter++])
-            throw std::exception("A wrong parameter");
-    }
+    std::vector<Variable::Type> arg_types;
 
-    _function(args);
+    if (!_argument_types[arg_types])
+        throw std::exception("Wrong parameters"); 
+
+    _function[arg_types](args);
 }
 
 
