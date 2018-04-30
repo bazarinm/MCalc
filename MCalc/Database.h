1fdc1ce7ed133133d4b2362fba8dbb084405747e
Михаил Базарин <mabazarin@edu.hse.ru>
2018-05-01 01:20:39 +0300

    refactor

--- MCalc/Database.h
+++ MCalc/Database.h
@@ -23,38 +23,39 @@ struct FunctionInfo {
     > _function;
 };
 
-std::map<std::string, FunctionInfo> database = {
-    { 
-        "*",
-        {
-            //arity = 
-            2,
-            //priority = 
-            2,
-            //invocation = 
-            FunctionInfo::OPERATOR,
-            //associativity = 
-            FunctionInfo::BOTH,
-            //parameters = 
-            std::vector<std::vector<Variable::Type>>
-            ({
-                { Variable::MATRIX, Variable::MATRIX },
-                { Variable::SCALAR, Variable::SCALAR },
-                { Variable::MATRIX, Variable::SCALAR },
-                { Variable::SCALAR, Variable::MATRIX }
-            }),
-    //functions:
-
-            std::map<std::vector<Variable::Type>, std::function<Variable(std::vector<Variable>)>>
-            ({
-                {
-                    { Variable::MATRIX, Variable::MATRIX },
-                    [](std::vector<Variable> args) -> Variable { return args[0].matrix() * args[1].matrix(); }
-                }
-            })
-        }
-    }
-}
+//std::map<std::string, FunctionInfo> database = {
+//    { 
+//        //name = 
+//        "*",
+//        {
+//            //arity = 
+//            2,
+//            //priority = 
+//            2,
+//            //invocation = 
+//            FunctionInfo::OPERATOR,
+//            //associativity = 
+//            FunctionInfo::BOTH,
+//            //parameters = 
+//            std::vector<std::vector<Variable::Type>>
+//            ({
+//                { Variable::MATRIX, Variable::MATRIX },
+//                { Variable::SCALAR, Variable::SCALAR },
+//                { Variable::MATRIX, Variable::SCALAR },
+//                { Variable::SCALAR, Variable::MATRIX }
+//            }),
+//            //functions:
+//
+//            std::map<std::vector<Variable::Type>, std::function<Variable(std::vector<Variable>)>>
+//            ({
+//                {
+//                    { Variable::MATRIX, Variable::MATRIX },
+//                    [](std::vector<Variable> args) -> Variable { return args[0].matrix() * args[1].matrix(); }
+//                }
+//            })
+//        }
+//    }
+//}
 
 
 #endif
