6095a1fa72be14e7c72df4c815c91a1029621145
Михаил Базарин <mabazarin@edu.hse.ru>
2018-05-01 01:13:18 +0300

    refactor

--- MCalc/Database.h
+++ MCalc/Database.h
@@ -23,39 +23,38 @@ struct FunctionInfo {
     > _function;
 };
 
-std::map<std::string, FunctionInfo> database;
-
-int initialize() {
-    database["*"] = {
-        //arity = 
-        2,
-        //priority = 
-        2,
-        //invocation = 
-        FunctionInfo::OPERATOR,
-        //associativity = 
-        FunctionInfo::BOTH,
-        //parameters = 
-        std::vector<std::vector<Variable::Type>>({
-            { Variable::MATRIX, Variable::MATRIX },
-            { Variable::SCALAR, Variable::SCALAR },
-            { Variable::MATRIX, Variable::SCALAR },
-            { Variable::SCALAR, Variable::MATRIX }
-            }),
-        //functions:
-
-        std::map<
-        std::vector<Variable::Type>, //left to right
-        std::function<Variable(std::vector<Variable>)>
-        >({
-            {
+std::map<std::string, FunctionInfo> database = {
+    { 
+        "*",
+        {
+            //arity = 
+            2,
+            //priority = 
+            2,
+            //invocation = 
+            FunctionInfo::OPERATOR,
+            //associativity = 
+            FunctionInfo::BOTH,
+            //parameters = 
+            std::vector<std::vector<Variable::Type>>
+            ({
                 { Variable::MATRIX, Variable::MATRIX },
-                [](std::vector<Variable> args) -> Variable { return args[0].matrix() * args[1].matrix(); }
-            }
-        })
-    };
+                { Variable::SCALAR, Variable::SCALAR },
+                { Variable::MATRIX, Variable::SCALAR },
+                { Variable::SCALAR, Variable::MATRIX }
+            }),
+    //functions:
 
-    return 0;
+            std::map<std::vector<Variable::Type>, std::function<Variable(std::vector<Variable>)>>
+            ({
+                {
+                    { Variable::MATRIX, Variable::MATRIX },
+                    [](std::vector<Variable> args) -> Variable { return args[0].matrix() * args[1].matrix(); }
+                }
+            })
+        }
+    }
 }
+
 
 #endif
